02ee14a8243f1e860e46392f3332b1fe8e88f3e0
bernard <bernard@arhivpro.hr>
2020-07-26 21:34:31 +0200

    fix: imports

--- experiment/mpu9250.py
+++ experiment/mpu9250.py
@@ -1,13 +1,13 @@
-import FaBo9Axis_MPU9250
-import time
 # to import from parent directory
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.abspath('../emitter'))
 
-from emitter.DataEmitter import DataEmitter
+import time
+import FaBo9Axis_MPU9250
+from DataEmitter import DataEmitter
+
 
 mpu9250 = FaBo9Axis_MPU9250.MPU9250()
-
 em = DataEmitter(debug=True)
 while True:
     acc = mpu9250.readAccel()
