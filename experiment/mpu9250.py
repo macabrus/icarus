162a99886e32066562eafbf05b23a4adc45ff4b9
bernard <bernard@arhivpro.hr>
2020-08-11 21:13:08 +0200

    fmdsklgmldflfdmg

--- experiment/mpu9250.py
+++ experiment/mpu9250.py
@@ -12,20 +12,28 @@ em = DataEmitter(debug=False)
 
 err = (0,0,0)
 
-while True:
-    acc = mpu9250.readAccel()
-    #gyro = mpu9250.readGyro()
-    #mag = mpu9250.readMagnet()
-    #temp = mpu9250.readTemperature()
-    #print({"accX":acc["x"],"accY":acc["y"],"accZ":acc["z"],
-    #         "magX":mag["x"],"magY":mag["y"],"magZ":mag["z"],
-    #         "gyrX":gyro["x"],"gyrY":gyro["y"],"gyrZ":gyro["z"],
-    #         "temp": temp})
-    res = balance(acc['x'] - err[0], acc['y'] - err[1], 1)
-    #print(("{:1.4f} "*7).format(acc['x'], acc['y'], acc['z'], *balance(acc['x'], acc['y'], 1)))
-    pi.set_servo_pulsewidth(esc1, scale(res[1], 1030, 1100))
-    pi.set_servo_pulsewidth(esc2, scale(res[0], 1030, 1100))
-    pi.set_servo_pulsewidth(esc3, scale(res[2], 1030, 1100))
-    pi.set_servo_pulsewidth(esc4, scale(res[3], 1030, 1100))
-    time.sleep(0.1)
-    #time.sleep(0.1155)
+lower = 1030
+
+def thread_job():
+    while True:
+        acc = mpu9250.readAccel()
+        #gyro = mpu9250.readGyro()
+        #mag = mpu9250.readMagnet()
+        #temp = mpu9250.readTemperature()
+        #print({"accX":acc["x"],"accY":acc["y"],"accZ":acc["z"],
+        #         "magX":mag["x"],"magY":mag["y"],"magZ":mag["z"],
+        #         "gyrX":gyro["x"],"gyrY":gyro["y"],"gyrZ":gyro["z"],
+        #         "temp": temp})
+        res = balance(acc['x'] - err[0], acc['y'] - err[1], 1)
+        #print(("{:1.4f} "*7).format(acc['x'], acc['y'], acc['z'], *balance(acc['x'], acc['y'], 1)))
+        pi.set_servo_pulsewidth(esc1, scale(res[1], lower, 1100))
+        pi.set_servo_pulsewidth(esc2, scale(res[0], lower, 1100))
+        pi.set_servo_pulsewidth(esc3, scale(res[2], lower, 1100))
+        pi.set_servo_pulsewidth(esc4, scale(res[3], lower, 1100))
+        time.sleep(0.1)
+        #time.sleep(0.1155)
+
+from threading import Thread
+
+Thread(target=thread_job).start()
+i = input()
